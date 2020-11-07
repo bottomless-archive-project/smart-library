25d8bbc2f826dd383207f2c114d43e011b451f08
Gyula Lakatos <laxika91@gmail.com>
2020-11-07 12:29:06 +0100

    #1 - Initialized the project with Spring Boot.

---
+++ smartlibrary-runtime/src/main/java/com/github/bottomlessarchive/smartlibrary/SmartLibraryApplication.java
@@ -0,0 +1,12 @@
+package com.github.bottomlessarchive.smartlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartLibraryApplication.class, args);
+	}
+}
